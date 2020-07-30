a7448f00aa7004aed1fed571924a7b09f2633d69
Sjory3 <mgj32000@gmail.com>
2020-07-30 08:53:31 +0200

    Update test.py
    tested soup

--- Python/test.py
+++ Python/test.py
@@ -1,1 +1,10 @@
+import requests
+from bs4 import BeautifulSoup
+
+page = requests.get("https://dk.grundfos.com/om-os/karriere/jobs/job.html?jobid=46029")
+
+data = page.text
+soup = BeautifulSoup(data, 'html.parser')
+
+print(soup.prettify())
 
