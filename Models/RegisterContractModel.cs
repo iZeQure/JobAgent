d1bd6c6cfa24cca879a3b0668162cc4693c2dbbb
iZeQure <iZeQure@users.noreply.github.com>
2020-09-02 22:42:56 +0200

    [Feature] - 1.1.6
    Adding features to the contract model, to provide the best way to provide easy tasks for the user.

--- Models/RegisterContractModel.cs
+++ Models/RegisterContractModel.cs
@@ -8,6 +8,9 @@ namespace JobAgent.Models
 {
     public class RegisterContractModel
     {
+        private DateTime registrationDateTime = DateTime.UtcNow;
+        private DateTime expiryDate;
+
         [Required]
         public string ContactPerson { get; set; }
 
@@ -15,15 +18,21 @@ namespace JobAgent.Models
         public string ContractFileName { get; set; }
 
         [Required]
-        public DateTime RegistrationDate { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
+        public DateTime RegistrationDate { get { return registrationDateTime; } set { registrationDateTime = value; } }
 
         [Required]
-        public DateTime ExpiryDate { get; set; }
+        [DataType(DataType.DateTime)]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:dd/MM/yyyy HH:mm}")]
+        public DateTime ExpiryDate { get { return registrationDateTime.AddYears(5); } set { expiryDate = value; } }
 
-        [Required]
+        [Required(ErrorMessage = "Vælg venligst en konsulent fra listen.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Vælg venligst en konsulent fra listen.")]
         public int SignedByUser { get; set; }
 
-        [Required]
+        [Required(ErrorMessage = "Vælg venligst en virksomhed fra listen.")]
+        [Range(1, int.MaxValue, ErrorMessage = "Vælg venligst en virksomhed fra listen.")]
         public int SignedWithCompany { get; set; }
     }
 }
