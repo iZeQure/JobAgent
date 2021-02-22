6939d62c97e186d32ec2dfa48038cbc07f257ab9
iZeQure <iZeQure@users.noreply.github.com>
2021-02-22 16:10:12 +0100

    [Website] - v1.6.0
    - UI got to match a better color scheme.
    - Fixed async call from other pages / components.
    - Fixed calls & backend flow from some of the admin pages.
    - Random crashes no longer occurs on specific pages, when trying to load data.

--- Website/Services/DataService.cs
+++ Website/Services/DataService.cs
@@ -15,62 +15,62 @@ namespace JobAgent.Services
             DataAccessManager = new DataAccessManager();
         }
 
-        public async Task<int> GetCountOfJobAdvertsByCategory(int id)
+        public async Task<int> GetCountOfJobAdvertsByCategoryAsync(int id)
         {
             return await DataAccessManager.JobAdvertDataAccessManager().GetCountOfJobAdvertsByCategoryId(id);
         }
 
-        public async Task<int> GetCountOfJobAdvertsBySpecializationId(int id)
+        public async Task<int> GetCountOfJobAdvertsBySpecializationIdAsync(int id)
         {
             return await DataAccessManager.JobAdvertDataAccessManager().GetCountOfJobAdvertsBySpecializationId(id);
         }
 
-        public async Task<int> GetCountOfJobAdvertsUncategorized()
+        public async Task<int> GetCountOfJobAdvertsUncategorizedAsync()
         {
             return await DataAccessManager.JobAdvertDataAccessManager().GetCountOfJobAdvertsUncategorized();
         }
 
-        public async Task<IEnumerable<ConsultantArea>> GetAllConsultantAreasTask()
+        public async Task<IEnumerable<ConsultantArea>> GetAllConsultantAreasAsync()
         {
             return await DataAccessManager.ConsultantAreaDataAccessManager().GetAll();
         }
 
-        public async Task<IEnumerable<Location>> GetAllLocationsTask()
+        public async Task<IEnumerable<Location>> GetAllLocationsAsync()
         {
             return await DataAccessManager.LocationDataAccessManager().GetAll();
         }
 
-        public async Task<IEnumerable<Category>> GetAllCategories()
+        public async Task<IEnumerable<Category>> GetAllCategoriesAsync()
         {
             return await DataAccessManager.CategoryDataAccessManager().GetAllCategories();
         }
 
-        public async Task<IEnumerable<Specialization>> GetAllSpecializations()
+        public async Task<IEnumerable<Specialization>> GetAllSpecializationsAsync()
         {
             return await DataAccessManager.CategoryDataAccessManager().GetAllSpecializations();
         }
 
-        public async Task<IEnumerable<Company>> GetAllCompanies()
+        public async Task<IEnumerable<Company>> GetAllCompaniesAsync()
         {
             return await DataAccessManager.CompanyDataAccessManager().GetAll();
         }
 
-        public async Task<IEnumerable<Company>> GetCompaniesWithContract()
+        public async Task<IEnumerable<Company>> GetCompaniesWithContractAsync()
         {
             return await DataAccessManager.CompanyDataAccessManager().GetCompaniesWithContract();
         }
 
-        public async Task<IEnumerable<Company>> GetCompaniesWithOutContract()
+        public async Task<IEnumerable<Company>> GetCompaniesWithOutContractAsync()
         {
             return await DataAccessManager.CompanyDataAccessManager().GetCompaniesWithOutContract();
         }
 
-        public async Task<Company> GetCompanyById(int id)
+        public async Task<Company> GetCompanyByIdAsync(int id)
         {
             return await DataAccessManager.CompanyDataAccessManager().GetById(id);
         }
 
-        public async Task<IEnumerable<User>> GetUsers()
+        public async Task<IEnumerable<User>> GetUsersAsync()
         {
             return await DataAccessManager.UserDataAccessManager().GetAll();
         }
@@ -80,7 +80,7 @@ namespace JobAgent.Services
             return await DataAccessManager.SourceLinkDataAccessManager().GetAll();
         }
 
-        public async Task<SourceLink> GetSourceLinkById(int id)
+        public async Task<SourceLink> GetSourceLinkByIdAsync(int id)
         {
             return await DataAccessManager.SourceLinkDataAccessManager().GetById(id);
         }
