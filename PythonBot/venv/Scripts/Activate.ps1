a7c9a6d6fc8cebb3611a644ee0bc097fc7e4a1c9
iZeQure <iZeQure@users.noreply.github.com>
2021-02-23 09:37:00 +0100

    [Crawler] - v0.2.0
    Tried fixing venv with python.exe.

--- PythonBot/venv/Scripts/Activate.ps1
+++ PythonBot/venv/Scripts/Activate.ps1
@@ -27,7 +27,7 @@ function global:deactivate ([switch]$NonDestructive) {
 
 deactivate -nondestructive
 
-$env:VIRTUAL_ENV="C:\Users\chri45i4\PycharmProjects\JobAhentCrawler\venv"
+$env:VIRTUAL_ENV="F:\Projects\github_repos\work\JobAgent\PythonBot\venv"
 
 if (! $env:VIRTUAL_ENV_DISABLE_PROMPT) {
     # Set the prompt to include the env name
