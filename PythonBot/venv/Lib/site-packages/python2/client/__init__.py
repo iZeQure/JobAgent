5461d754a1b94a10261225356d957300087ea0af
iZeQure <TobiasRosenvinge@hotmail.com>
2021-03-19 10:37:30 +0100

    [Crawler] - 0.9.0
    Removed Project due to corruption. Added new files, trying to fix it.

--- PythonBot/venv/Lib/site-packages/python2/client/__init__.py
+++
@@ -1,5 +0,0 @@
-# Convenience imports
-
-from python2.client.exceptions import Py2Error  # noqa
-from python2.client.object import Py2Object  # noqa
-from python2.client.session import Python2  # noqa
