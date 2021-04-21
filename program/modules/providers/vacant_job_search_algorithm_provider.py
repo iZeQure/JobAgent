c43629fc85402fe9a79d36c0d1857ea2fbbc515d
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-04-21 09:23:50 +0200

    Renamed File
    Added Search to the name of the provider class.

--- program/modules/providers/vacant_job_search_algorithm_provider.py
+++ program/modules/providers/vacant_job_search_algorithm_provider.py
@@ -1,3 +1,4 @@
+from re import compile
 from program.modules.services.data_service import DataService
 from program.modules.providers.search_algorithm_provider import SearchAlgorithmProvider
 
@@ -5,3 +6,11 @@ from program.modules.providers.search_algorithm_provider import SearchAlgorithmP
 class VacantJobSearchAlgorithmProvider(SearchAlgorithmProvider):
     def __init__(self, data_service: DataService):
         super().__init__(data_service)
+
+    def find_vacant_job_links(self) -> []:
+        useful_links = []
+        for link in self.soup.find_all('a'):
+            if link.text:
+                useful_links.append(link['href'])
+
+        return
