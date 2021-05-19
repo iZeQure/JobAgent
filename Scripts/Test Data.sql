486775bad013f44e7f6561fbb1891941d65a7439
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-05-19 14:13:36 +0200

    Test Data
    Made a new file with very little data to test the robot with.

---
+++ Scripts/Test Data.sql
@@ -0,0 +1,9 @@
+INSERT INTO [Company] ([CVR],[Name],[ContactPerson])
+VALUES 
+(62786515,'DR','Ikke Nogen'),
+(1, 'Praktikpladsen', 'Staten');
+
+INSERT INTO [JobPage] ([CompanyId],[URL])
+VALUES
+(1, 'https://www.dr.dk/tjenester/job-widget/'),
+(2, 'https://pms.praktikpladsen.dk/soeg-opslag/0/Data-%20og%20kommunikationsuddannelsen/Datatekniker%20med%20speciale%20i%20programmering');
