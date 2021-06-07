bd7d595f47cf7d61f2c6e18f48329e2fda018946
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-06-07 16:21:25 +0200

    Started new Project.
    Started on implementing the new design for version 2.0. Almost done with the landing page, just gotta fix the position of the background image to match the remaining space.

---
+++ WebsiteV2/Data/Models/AuthenticationModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Net.Mail;
+using System.Threading.Tasks;
+
+namespace WebsiteV2.Data.Models
+{
+    public class AuthenticationModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email adresse er påkrævet.")]
+        [StringLength(255, ErrorMessage = "Email er for long (255 karakter begrænse).")]
+        [EmailAddress(ErrorMessage = "Indtast en gyldig email adresse.")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Adgangskode er påkrævet.")]
+        [StringLength(255)]
+        public string Password { get; set; }
+
+        public bool IsValidEmail(string email)
+        {
+            try
+            {
+                var addr = new MailAddress(email);
+                return addr.Address == email;
+            }
+            catch
+            {
+                return false;
+            }
+        }
+    }
+}
