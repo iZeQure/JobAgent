bc22b0dd031b69d896408399ad4b17e619cb7ab7
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-06-10 13:16:08 +0200

    Components
    Added new way to control our components for easier use of the pages who need the component.

---
+++ WebsiteV2/Shared/Components/Redirects/AdminPageRedirect.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteV2.Shared.Components.Redirects
+{
+    /// <summary>
+    /// Handles the redirection to Admin Page.
+    /// </summary>
+    public class AdminPageRedirect : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.NavigateTo("/admin", true);
+        }
+    }
+}
