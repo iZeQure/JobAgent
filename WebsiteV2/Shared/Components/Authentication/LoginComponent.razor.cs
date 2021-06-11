c0c13ac36088a3efb0ef3d2398b9a8e6ea1cfc0c
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-06-11 12:43:46 +0200

    Login View
    Been playing around with how to control the login backend, while also modifying the styling of display errors and success messages. Made a sprites files for accessible svgs for the display messages.

---
+++ WebsiteV2/Shared/Components/Authentication/LoginComponent.razor.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebsiteV2.Data.Models;
+
+namespace WebsiteV2.Shared.Components.Authentication
+{
+    public partial class LoginComponent : ComponentBase
+    {
+        private EditContext _authContext;
+        private readonly AuthenticationModel _authenticationModel = new();
+        private bool _isLogInDisabled = true;
+
+        public string ValidationMessage { get; set; }
+
+        protected override Task OnInitializedAsync()
+        {
+            _authContext = new EditContext(_authenticationModel);
+            _authContext.AddDataAnnotationsValidation();
+
+            return base.OnInitializedAsync();
+        }        
+
+        private Task OnValidSubmit_LogInAsync()
+        {
+            ValidationMessage = "Logger ind, vent venligst..";
+
+            return ClearValidationMessageAfterInterval();
+        }
+
+        private Task OnInvalidSubmit_DisplayError()
+        {
+            ValidationMessage = "Der er fejl i formen, tjek venligst.";
+
+            return ClearValidationMessageAfterInterval();
+        }
+
+        private Task ClearValidationMessageAfterInterval(int milliseconds = 3000)
+        {
+            var x = Task.Delay(3000)
+                .ContinueWith(x =>
+                {
+                    ValidationMessage = string.Empty;
+                    return x.IsCompleted;
+                });
+
+            return x;
+        }
+    }
+}
