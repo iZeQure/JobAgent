bd0ec7e35db9740302a6e560b4269224bfae9b9f
iZeQure <TobiasRosenvinge@hotmail.com>
2021-06-17 14:30:43 +0200

    Finished My Profile Section
    Finished the designing of the my profile section, made a form context model for the form and then changed all the proper validation fields with proper error messages.

---
+++ WebsiteV2/Data/FormModels/AccountProfileModel.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebsiteV2.Data.FormModels
+{
+    public class AccountProfileModel
+    {
+        [Required]
+        public int AccountId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email adresse er påkrævet.")]
+        [StringLength(maximumLength: 255, MinimumLength = 1)]
+        [EmailAddress(ErrorMessage = "Indtast en gyldig email adresse.")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Udfyld venligst fornavn.")]
+        [StringLength(maximumLength: 128, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Udfyld venligst efternavn.")]
+        [StringLength(maximumLength: 128, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Required(ErrorMessage = "Vælg venligst en eller flere områder fra listen.")]
+        [MinLength(1, ErrorMessage = "Vælg venligst en eller flere områder fra listen.")]
+        public List<int> ConsultantAreaIds { get; set; }
+
+        [Required]
+        [Range(1 , 1, ErrorMessage = "Vælg venligst en lokation fra listen.")]
+        public int LocationId { get; set; }
+    }
+}
---
+++ WebsiteV2/Pages/Admin/Account/MyProfile.razor.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Forms;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using WebsiteV2.Data.FormModels;
+
+namespace WebsiteV2.Pages.Admin.Account
+{
+    public partial class MyProfile : ComponentBase
+    {
+        private EditContext _editContext;
+        private AccountProfileModel _accountProfileModel = new();
+
+        protected override Task OnInitializedAsync()
+        {
+            _editContext = new(_accountProfileModel);
+            _editContext.AddDataAnnotationsValidation();
+
+            return base.OnInitializedAsync();
+        }
+
+        private Task OnValidSubmit_ChangeUserInformation()
+        {
+            if (_accountProfileModel.AccountId == 0)
+            {
+                return Task.CompletedTask;
+            }
+
+            return Task.CompletedTask;
+        }
+    }
+}
