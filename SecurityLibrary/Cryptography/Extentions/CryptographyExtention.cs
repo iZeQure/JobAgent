55b6565d2a68d653452c63114ecb7b8c4fec7dee
iZeQure <TobiasRosenvinge@hotmail.com>
2021-06-21 23:32:06 +0200

    Extension Methods
    Created some extension methods for the user, whenever the user is called by it's initialized object and the reference to the namespace of the cryptography extension is used, then it's possible to use these methods as part of the user itself.

---
+++ SecurityLibrary/Cryptography/Extentions/CryptographyExtention.cs
@@ -0,0 +1,61 @@
+﻿using ObjectLibrary.Common;
+using System;
+using System.Security.Cryptography;
+using System.Text;
+
+namespace SecurityLibrary.Cryptography.Extentions
+{
+    public static class CryptographyExtention
+    {
+        /// <summary>
+        /// Hashes the given password of the specified user.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns>A hashed password.</returns>
+        public static string HashPassword(this User value)
+        {
+            try
+            {
+                byte[] encodedBytes = Encoding.UTF8.GetBytes(value.Password + value.Salt);
+                byte[] computedHashCode = new SHA384Managed().ComputeHash(encodedBytes);
+
+                return Convert.ToBase64String(computedHashCode);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Generates a salt, salt length is determined by the length of the password.
+        /// </summary>
+        /// <param name="value"></param>
+        /// <exception cref="ArgumentException">Is thrown if password is null or empty.</exception>
+        /// <exception cref="ArgumentNullException">Is thrown when the converted array is null.</exception>
+        /// <returns>A generated salt.</returns>
+        public static string GenerateSalt(this User value)
+        {
+            try
+            {
+                if (string.IsNullOrEmpty(value.Password))
+                {
+                    throw new ArgumentException("Could'n generate salt. Password was undefined.");
+                }
+
+                byte[] buffer = new byte[value.Password.Length];
+
+                using (var rng = new RNGCryptoServiceProvider())
+                {
+                    rng.GetNonZeroBytes(buffer);
+                }
+
+                return Convert.ToBase64String(buffer);
+            }
+            catch (ArgumentNullException)
+            {
+                throw;
+            }
+        }
+    }
+}
