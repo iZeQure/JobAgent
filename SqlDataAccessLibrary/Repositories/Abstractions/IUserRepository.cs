3dcdf8fa18e2328c1c6b1f5316e9dcc4a803ab53
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-22 15:00:36 +0200

    Update IUserRepository.cs

--- SqlDataAccessLibrary/Repositories/Abstractions/IUserRepository.cs
+++ SqlDataAccessLibrary/Repositories/Abstractions/IUserRepository.cs
@@ -3,18 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SqlDataAccessLibrary.Repositories.Abstractions
 {
     public interface IUserRepository : IRepository<User>
     {
-        Task<IUser> AuthenticateUserLoginAsync(User user);
+        Task<IUser> AuthenticateUserLoginAsync(User user, CancellationToken cancellation);
 
-        Task<User> GetUserByAccessTokenAsync(string accessToken);
+        Task<User> GetUserByAccessTokenAsync(string accessToken, CancellationToken cancellation);
 
-        Task<bool> CheckUserExistsAsync(User user);
+        Task<bool> CheckUserExistsAsync(User user, CancellationToken cancellation);
 
-        Task<int> UpdateUserPasswordAsync(User user);
+        Task<int> UpdateUserPasswordAsync(User user, CancellationToken cancellation);
     }
 }
