26f7ad7a14806d88b52c218d2c9d9e1defaac603
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-06-22 12:30:37 +0200

    Sql Helper pattern
    Made the sql database logic to match a helper pattern to make it easier to dynamically access the database as needed.

--- SqlDataAccessLibrary/Database/SqlDatabase.cs
+++ SqlDataAccessLibrary/Database/SqlDatabase.cs
@@ -1,11 +1,7 @@
 ﻿using ObjectLibrary.Common.Configuration;
 using System;
-using System.Collections.Generic;
 using System.Data;
-using System.Data.Common;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -16,53 +12,21 @@ namespace SqlDataAccessLibrary.Database
     /// </summary>
     public class SqlDatabase : ISqlDatabase
     {
-        private readonly IDbConnection _dbConnection;
+        private readonly IConfigurationSettings _settings;
 
         public SqlDatabase(IConfigurationSettings settings)
         {
-            _dbConnection = new SqlConnection(settings.ConnectionString);
+            _settings = settings;
         }
 
-        /// <summary>
-        /// Opens a new connection asynchronously.
-        /// </summary>
-        /// <param name="cancellation">A token representing the task to cancel if needed.</param>
-        /// <returns>A task representing the asynchronous progress.</returns>
-        public async Task OpenConnectionAsync(CancellationToken cancellation)
+        public async Task<int> ExecuteNonQueryAsync(string commandText, CommandType commandType, CancellationToken cancellation, params SqlParameter[] parameters)
         {
-            if (_dbConnection is SqlConnection connection)
-            {
-                using (connection)
-                {
-                    if (connection.State != ConnectionState.Open)
-                    {
-                        try
-                        {
-                            await connection.OpenAsync(cancellation);
-                        }
-                        catch (Exception ex)
-                        {
-                            await Task.FromException(ex);
-                        }
-                    }
-                }
-
-                await Task.CompletedTask;
-            }
-
-            await Task.FromException(new Exception("Error: 600. Could not initialize a valid connection."));
+            return await SqlHelper.ExecuteNonQueryAsync(_settings.ConnectionString, commandText, commandType, cancellation, parameters);
         }
 
-        public void Dispose()
+        public async Task<SqlDataReader> ExecuteReaderAsync(string commandText, CommandType commandType, CancellationToken cancellation, params SqlParameter[] parameters)
         {
-            if (_dbConnection.State == ConnectionState.Closed)
-            {
-                return;
-            }
-
-            _dbConnection.Close();
-
-            GC.SuppressFinalize(this);
+            return await SqlHelper.ExecuteReaderAsync(_settings.ConnectionString, commandText, commandType, cancellation, parameters);
         }
     }
 }
