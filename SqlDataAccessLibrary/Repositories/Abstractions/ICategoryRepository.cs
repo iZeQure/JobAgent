b109d5b9f82d713268fb144ac3279ce773f562a9
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-23 12:27:46 +0200

    Repo interface Update
    Added methods to [ICategoryRepository], [ICompanyRepository], and [ISpecializationRepository]

--- SqlDataAccessLibrary/Repositories/Abstractions/ICategoryRepository.cs
+++ SqlDataAccessLibrary/Repositories/Abstractions/ICategoryRepository.cs
@@ -3,11 +3,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SqlDataAccessLibrary.Repositories.Abstractions
 {
     public interface ICategoryRepository : IRepository<Category>
     {
+        /// <summary>
+        /// Get all categories with specializations bound to.
+        /// </summary>
+        /// <returns>Returns a list of <see cref="Category"/>, with a list of <see cref="Specialization"/>, if the category owns a specialization.</returns>
+        Task<IEnumerable<Category>> GetAllCategoriesWithSpecializations(CancellationToken cancellation);
+
+        Task<IEnumerable<Category>> GetAllCategories(CancellationToken cancellation);
     }
 }
--- SqlDataAccessLibrary/Repositories/Abstractions/ICompanyRepository.cs
+++ SqlDataAccessLibrary/Repositories/Abstractions/ICompanyRepository.cs
@@ -3,11 +3,16 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace SqlDataAccessLibrary.Repositories.Abstractions
 {
     public interface ICompanyRepository : IRepository<Company>
     {
+        public Task<IEnumerable<Company>> GetCompaniesWithContract(CancellationToken cancellation);
+
+        public Task<IEnumerable<Company>> GetCompaniesWithOutContract(CancellationToken cancellation);
+
     }
 }
