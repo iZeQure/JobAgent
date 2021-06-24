89c2eb8085e789869f4aa5a325cbfb9abe0b7363
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-24 14:15:59 +0200

    Loggin/Versioning Repo Interfaces
    Added Interfaces for Logging and Versioning Repositories

---
+++ SqlDataAccessLibrary/Repositories/Abstractions/IVersionControlRepository.cs
@@ -0,0 +1,10 @@
+﻿using ObjectLibrary.Versioning;
+
+
+namespace SqlDataAccessLibrary.Repositories.Abstractions
+{
+    public interface IVersionControlRepository : IRepository<VersionControl>
+    {
+
+    }
+}
