2b9689c139fda2172a79559fc83771a3a075c582
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-24 08:50:33 +0200

    Usings Update
    Removed unused sortings

--- SqlDataAccessLibrary/StaticSearchFilterRepository.cs
+++ SqlDataAccessLibrary/StaticSearchFilterRepository.cs
@@ -1,13 +1,10 @@
-﻿using ObjectLibrary.Common;
-using ObjectLibrary.Filter;
+﻿using ObjectLibrary.Filter;
 using SqlDataAccessLibrary.Database;
 using SqlDataAccessLibrary.Repositories.Abstractions;
 using System;
 using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
