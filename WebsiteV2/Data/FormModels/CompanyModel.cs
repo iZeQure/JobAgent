e4d630eef6e3a1c53694d6ab787e9d97c55bc8df
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-28 13:56:05 +0200

    Company page logic
    added CompanyModel
    
    Added company.razor.cs

---
+++ WebsiteV2/Data/FormModels/CompanyModel.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerWebsite.Data.FormModels
+{
+    public class CompanyModel
+    {
+        [Required]
+        public int CompanyId { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "CVR er påkrævet")]
+        [StringLength(maximumLength: 255, MinimumLength = 1)]
+        public int CVR { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Firma Navn er påkrævet")]
+        [StringLength(maximumLength: 255, MinimumLength = 1)]
+        public string Name { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "En Kontakt Person er påkrævet")]
+        public string ContactPerson { get; set; }
+    }
+}
---
+++ WebsiteV2/Pages/Admin/Administrate/Company.razor.cs
@@ -0,0 +1,77 @@
+﻿using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerWebsite.Pages.Admin.Administrate
+{
+    public partial class Company : ComponentBase
+    {
+        //[Inject] protected DataService DataService { get; set; }
+        //[Inject] protected IJSRuntime JSRuntime { get; set; }
+        //[Inject] protected IRefresh RefreshService { get; set; }
+
+        private IEnumerable<Company> companies = new List<Company>();
+
+        private CompanyModel model { get; set; } = new CompanyModel();
+        private int companyId { get; set; }
+        private string errorMessage = string.Empty;
+        private bool dataIsLoading = false;
+
+        protected override async Task OnInitializedAsync()
+        {
+            RefreshService.RefreshRequest += UpdateContentAsync;
+
+            companies = await LoadData();
+
+            dataIsLoading = false;
+        }
+
+        private async Task<IEnumerable<Company>> LoadData()
+        {
+            dataIsLoading = true;
+
+            return await DataService.GetAllCompaniesAsync();
+        }
+
+        private async Task UpdateContentAsync()
+        {
+            try
+            {
+                var companies = await DataService.GetAllCompaniesAsync();
+
+                if (companies != null)
+                {
+                    this.companies = companies;
+                    return;
+                }
+
+                this.companies = null;
+            }
+            catch (Exception) { errorMessage = "Ukendt Fejl ved opdatering af virksomheder."; }
+            finally { StateHasChanged(); }
+        }
+
+        private async void OnClick_OpenEditModal(int id)
+        {
+            var company = await DataService.GetCompanyByIdAsync(id);
+
+            model = new CompanyModel()
+            {
+                Id = company.Identifier,
+                CVR = company.CVR,
+                Name = company.Name,
+                URL = company.URL
+            };
+        }
+
+        private void OnClick_RemoveCompanyModal(int id)
+        {
+            companyId = id;
+        }
+
+
+    }
+}
