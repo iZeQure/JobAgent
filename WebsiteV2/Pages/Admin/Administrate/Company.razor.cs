58ad310910f9a082a86d68f85723eff44b77ef17
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-28 13:56:39 +0200

    Update Company.razor.cs
    added usings

--- WebsiteV2/Pages/Admin/Administrate/Company.razor.cs
+++ WebsiteV2/Pages/Admin/Administrate/Company.razor.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Components;
+﻿using BlazorServerWebsite.Data.FormModels;
+using Microsoft.AspNetCore.Components;
 using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
