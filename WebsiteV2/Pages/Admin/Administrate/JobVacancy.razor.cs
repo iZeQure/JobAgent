1d8d52af5faa93f8c11fda2f7a37a223487f2a01
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-29 12:25:39 +0200

    page underclasses

---
+++ WebsiteV2/Pages/Admin/Administrate/JobVacancy.razor.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerWebsite.Pages.Admin.Administrate
+{
+    public partial class JobVacancy
+    {
+    }
+}
