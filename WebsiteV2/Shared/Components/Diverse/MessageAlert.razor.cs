8efba55b31a7039d4578db602b0fe3c5245760a2
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-29 11:05:32 +0200

    Shared Component update
    Added MessageAlert Component
    
    Added Popover Component

---
+++ WebsiteV2/Shared/Components/Diverse/MessageAlert.razor.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.AspNetCore.Components;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlazorServerWebsite.Components.Notification
+{
+    public partial class MessageAlert
+    {
+        [Parameter]
+        public string Message { get; set; } = string.Empty;
+        [Parameter]
+        public string MessageOptional { get; set; } = string.Empty;
+        [Parameter]
+        public AlertType Alert { get; set; }
+        [Parameter]
+        public bool IsLoading { get; set; } = false;
+        [Parameter]
+        public bool FullWidth { get; set; } = true;
+
+        public string Container
+        {
+            get
+            {
+                if (FullWidth)
+                    return "container-fluid";
+                else
+                    return "container";
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            StateHasChanged();
+        }
+
+        public enum AlertType { Info, Warning, Error, Success }
+    }
+}
