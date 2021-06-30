c76ae34b157f931c493ea56982fe7b3ff6baf55a
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-30 10:26:04 +0200

    RegisterUserPage Logic
    Added Back-end to RegistUser.razor.cs
    
    Added RegisterUserModel.cs

---
+++ WebsiteV2/Data/FormModels/RegisterUserModel.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BlazorServerWebsite.Data.FormModels
+{
+    public class RegisterUserModel
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Udfyld venligst fornavn.")]
+        [StringLength(maximumLength: 128, MinimumLength = 1)]
+        public string FirstName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Udfyld venligst efternavn.")]
+        [StringLength(maximumLength: 128, MinimumLength = 1)]
+        public string LastName { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Email adresse er påkrævet.")]
+        [StringLength(maximumLength: 255, MinimumLength = 1)]
+        [EmailAddress(ErrorMessage = "Indtast en gyldig email adresse.")]
+        public string Email { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Adgangskode er påkrævet.")]
+        [StringLength(255, ErrorMessage = "Adgangskode er for lang eller kort.", MinimumLength = 6)]
+        [PasswordPropertyText]
+        public string Password { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Genindtast din adgangskode.")]
+        [Compare(nameof(Password), ErrorMessage = "Adgangskode stemmer ikke overens.")]
+        [StringLength(255, ErrorMessage = "Adgangskode er for lang eller kort.", MinimumLength = 6)]
+        [PasswordPropertyText]
+        public string ConfirmPassword { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Vælg venligst et konsulent område.")]
+        public int ConsultantAreaId { get; set; }
+
+        [Required]
+        [Range(1, int.MaxValue, ErrorMessage = "Vælg venligst en lokation.")]
+        public int LocationId { get; set; }
+    }
+}
--- WebsiteV2/Pages/Admin/Administrate/RegisterUser.razor.cs
+++ WebsiteV2/Pages/Admin/Administrate/RegisterUser.razor.cs
@@ -1,11 +1,133 @@
-﻿using System;
+﻿using BlazorServerWebsite.Data.FormModels;
+using BlazorServerWebsite.Data.Services;
+using BlazorServerWebsite.Data.Services.Abstractions;
+using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
+using ObjectLibrary.Common;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace BlazorServerWebsite.Pages.Admin.Administrate
 {
     public partial class RegisterUser
     {
+        [Inject] private IUserService UserService { get; set; }
+        [Inject] private IAreaService AreaService { get; set; }
+        [Inject] private ILocationService LocationService { get; set; }
+
+        private RegisterUserModel _regAccModel;
+        private User User;
+        private IEnumerable<Area> _areas;
+        private IEnumerable<Location> _locations;
+        private CancellationTokenSource _tokenSource = new();
+
+        private bool isProcessingRequest = false;
+        private bool dataIsLoading = false;
+        private bool errorOcurred = false;
+        private bool emailExists = false;
+        private string errorMessage = string.Empty;
+        private string infoMessage = string.Empty;
+
+        protected override async Task OnInitializedAsync()
+        {
+            _areas = new List<Area>();
+            _locations = new List<Location>();
+            _regAccModel = new();
+
+            await LoadInformation();
+        }
+
+        private async Task LoadInformation()
+        {
+            errorOcurred = false;
+            dataIsLoading = true;
+
+            try
+            {
+                _areas = await AreaService.GetAllAsync(_tokenSource.Token);
+                _locations = await LocationService.GetAllAsync(_tokenSource.Token);
+            }
+            catch (Exception)
+            {
+                errorOcurred = true;
+                errorMessage = "Kunne ikke hente nødvendig information, prøv igen senere.";
+            }
+            finally
+            {
+                dataIsLoading = false;
+            }
+        }
+
+        private async Task OnValidSubmit_RegisterAccountAsync()
+        {
+            ClearMessages();
+
+            errorOcurred = false;
+            isProcessingRequest = true;
+
+            try
+            {
+                if (emailExists)
+                {
+                    CancelRegistrationRequest();
+                    errorMessage = "Denne email er allerede i brug";
+                    return;
+                }
+
+                var userService = (UserService)UserService;
+
+                var userResult = await userService.CreateAsync(User, _tokenSource.Token);
+                await userService.GrantUserAreaAsync(User, _regAccModel.ConsultantAreaId, _tokenSource.Token);
+
+                infoMessage = "Praktikkonsulent blev oprettet succesfuldt";
+            }
+            catch (Exception)
+            {
+                errorOcurred = true;
+                errorMessage = "Ukendt Fejl.";
+            }
+            finally
+            {
+                isProcessingRequest = false;
+                _regAccModel = new();
+            }
+        }
+
+        private async Task OnEmailFocusOut_CheckForExistence(FocusEventArgs e)
+        {
+            try
+            {
+                var userService = (UserService)UserService;
+
+                if (userService != null)
+                {
+                    if (string.IsNullOrEmpty(_regAccModel.Email) || string.IsNullOrWhiteSpace(_regAccModel.Email))
+                    {
+                        emailExists = false;
+                        return;
+                    }
+
+                    emailExists = await userService.ValidateUserExistsByEmail(_regAccModel.Email, _tokenSource.Token);
+                }
+            }
+            catch (Exception)
+            {
+                errorMessage = "Fejl ved tjek om email findes.";
+            }
+        }
+
+        private void CancelRegistrationRequest()
+        {
+            errorMessage = "Praktikkonsulent eksisterer allerede.";
+            _regAccModel.Email = string.Empty;
+        }
+
+        private void ClearMessages()
+        {
+            infoMessage = string.Empty;
+            errorMessage = string.Empty;
+        }
     }
 }
