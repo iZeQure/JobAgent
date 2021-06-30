8249ba7eb90eb76613acf64e66f9c5083ad6bc48
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-06-30 11:25:09 +0200

    Job services

---
+++ WebsiteV2/Data/Services/JobAdvertService.cs
@@ -0,0 +1,43 @@
+﻿using BlazorServerWebsite.Data.Services.Abstractions;
+using ObjectLibrary.Common;
+using SqlDataAccessLibrary.Repositories.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace BlazorServerWebsite.Data.Services
+{
+    public class JobAdvertService : BaseService<IJobAdvertRepository, JobAdvert>, IJobAdvertService
+    {
+        public JobAdvertService(IJobAdvertRepository repository) : base(repository)
+        {
+        }
+
+        public async override Task<int> CreateAsync(JobAdvert createEntity, CancellationToken cancellation)
+        {
+            return await Repository.CreateAsync(createEntity, cancellation);
+        }
+
+        public async override Task<int> DeleteAsync(JobAdvert deleteEntity, CancellationToken cancellation)
+        {
+            return await Repository.DeleteAsync(deleteEntity, cancellation);
+        }
+
+        public async override Task<IEnumerable<JobAdvert>> GetAllAsync(CancellationToken cancellation)
+        {
+            return await Repository.GetAllAsync(cancellation);
+        }
+
+        public async override Task<JobAdvert> GetByIdAsync(int id, CancellationToken cancellation)
+        {
+            return await Repository.GetByIdAsync(id, cancellation);
+        }
+
+        public async override Task<int> UpdateAsync(JobAdvert updateEntity, CancellationToken cancellation)
+        {
+            return await Repository.UpdateAsync(updateEntity, cancellation);
+        }
+    }
+}
