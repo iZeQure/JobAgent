36b98822d54a91f7bda91456d9494f2bc8ab53e1
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-02 15:00:18 +0100

    Company Repo and Service Implementation

--- JobAgentSolutions/JobAgentClassLibrary/Common/Companies/Repositories/CompanyRepository.cs
+++ JobAgentSolutions/JobAgentClassLibrary/Common/Companies/Repositories/CompanyRepository.cs
@@ -1,35 +1,224 @@
 ﻿using JobAgentClassLibrary.Common.Companies.Entities;
+using JobAgentClassLibrary.Core.Database.Managers;
+using JobAgentClassLibrary.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace JobAgentClassLibrary.Common.Companies.Repositories
 {
     public class CompanyRepository : ICompanyRepository
     {
-        public Task<ICompany> CreateAsync(ICompany entity)
+        private readonly SqlDbManager _sqlDbManager;
+
+        public CompanyRepository(SqlDbManager sqlDbManager)
         {
-            throw new NotImplementedException();
+            _sqlDbManager = sqlDbManager;
         }
 
-        public Task<List<ICompany>> GetAllAsync(ICompany entity)
+
+        public async Task<ICompany> CreateAsync(ICompany entity)
         {
-            throw new NotImplementedException();
+            int entityId = 0;
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Create))
+            {
+                var values = new SqlParameter[]
+                {
+                    new SqlParameter("@companyCVR", entity.CVR),
+                    new SqlParameter("@companyName", entity.Name),
+                    new SqlParameter("@contactPerson", entity.ContactPerson)
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spCreateCompany]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+
+                    try
+                    {
+                        await conn.OpenAsync();
+
+                        entityId = (int)await cmd.ExecuteScalarAsync();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            if (entityId != 0)
+            {
+                return await GetByIdAsync(entityId);
+            }
+
+            return null;
         }
 
-        public Task<ICompany> GetByIdAsync(int id)
+        public async Task<List<ICompany>> GetAllAsync()
         {
-            throw new NotImplementedException();
+            List<ICompany> categories = new();
+
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Create))
+            {
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spGetCompanies]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    try
+                    {
+                        using (var reader = await cmd.ExecuteReaderAsync())
+                        {
+                            if (!reader.HasRows) return null;
+
+                            while (await reader.ReadAsync())
+                            {
+                                var company = new Company
+                                {
+                                    Id = reader.GetInt32(0),
+                                    CVR = reader.GetInt32(1),
+                                    Name = reader.GetString(2),
+                                    ContactPerson = reader.GetString(3)
+                                };
+
+                                categories.Add(company);
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            return categories;
         }
 
-        public Task<bool> RemoveAsync(ICompany entity)
+        public async Task<ICompany> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            Company company = new Company();
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Create))
+            {
+                var values = new SqlParameter[]
+                {
+                        new SqlParameter("@companyId", id)
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spGetCompanyById]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+
+                    try
+                    {
+                        using (var reader = await cmd.ExecuteReaderAsync())
+                        {
+                            if (!reader.HasRows) return null;
+
+                            while (await reader.ReadAsync())
+                            {
+                                company = new()
+                                {
+                                    Id = reader.GetInt32(0),
+                                    Name = reader.GetString(1)
+                                };
+
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return company;
         }
 
-        public Task<ICompany> UpdateAsync(ICompany entity)
+        public async Task<bool> RemoveAsync(ICompany entity)
         {
-            throw new NotImplementedException();
+            int entityId = 0;
+
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Create))
+            {
+                var values = new SqlParameter[]
+                {
+                        new SqlParameter("@companyId", entity.Id)
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spDeleteCompany]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+
+                    try
+                    {
+                        await conn.OpenAsync();
+
+                        entityId = (int)await cmd.ExecuteScalarAsync();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            if (entityId != 0)
+            {
+                return true;
+            }
+
+            return false;
         }
+
+        public async Task<ICompany> UpdateAsync(ICompany entity)
+        {
+            int entityId = 0;
+
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Create))
+            {
+                var values = new SqlParameter[]
+                {
+                        new SqlParameter("@id", entity.Id),
+                        new SqlParameter("@companyCVR", entity.CVR),
+                        new SqlParameter("@companyName", entity.Name),
+                        new SqlParameter("@contactPerson", entity.ContactPerson)
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spUpdateCompany]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+
+                    try
+                    {
+                        await conn.OpenAsync();
+
+                        entityId = (int)await cmd.ExecuteScalarAsync();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            if (entityId != 0)
+            {
+                return await GetByIdAsync(entityId);
+            }
+
+            return null;
+        }
+    
     }
 }
