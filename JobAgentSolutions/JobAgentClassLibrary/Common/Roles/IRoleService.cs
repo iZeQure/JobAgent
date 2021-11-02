1907207ee00747a14d3635bf2f2fa6c49f2208bd
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-11-02 15:53:46 +0100

    Implemented logic for Role.

--- JobAgentSolutions/JobAgentClassLibrary/Common/Roles/IRoleService.cs
+++ JobAgentSolutions/JobAgentClassLibrary/Common/Roles/IRoleService.cs
@@ -1,12 +1,15 @@
-﻿using System;
+﻿using JobAgentClassLibrary.Common.Roles.Entities;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 
 namespace JobAgentClassLibrary.Common.Roles
 {
     public interface IRoleService
     {
+        Task<IRole> CreateAsync(IRole entity);
+        Task<List<IRole>> GetRolesAsync();
+        Task<IRole> GetRoleByIdAsync(int id);
+        Task<bool> RemoveAsync(IRole entity);
+        Task<IRole> UpdateAsync(IRole entity);
     }
 }
