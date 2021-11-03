500d32e93ca2a3a38702b61303e3eed4266e528e
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-03 08:59:10 +0100

    Implemented JobAdvertRepository

--- JobAgentSolutions/JobAgentClassLibrary/Common/JobAdverts/Repositories/JobAdvertRepository.cs
+++ JobAgentSolutions/JobAgentClassLibrary/Common/JobAdverts/Repositories/JobAdvertRepository.cs
@@ -1,20 +1,109 @@
 ﻿using JobAgentClassLibrary.Common.JobAdverts.Entities;
+using JobAgentClassLibrary.Core.Database.Managers;
+using JobAgentClassLibrary.Core.Entities;
 using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Threading.Tasks;
 
 namespace JobAgentClassLibrary.Common.JobAdverts.Repositories
 {
     public class JobAdvertRepository : IJobAdvertRepository
     {
-        public Task<IJobAdvert> CreateAsync(IJobAdvert entity)
+        private readonly ISqlDbManager _sqlDbManager;
+
+        public JobAdvertRepository(ISqlDbManager sqlDbManager)
         {
-            throw new NotImplementedException();
+            _sqlDbManager = sqlDbManager;
         }
 
-        public Task<List<IJobAdvert>> GetAllAsync(IJobAdvert entity)
+
+        public async Task<IJobAdvert> CreateAsync(IJobAdvert entity)
         {
-            throw new NotImplementedException();
+            int entityId = 0;
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Create))
+            {
+                var values = new SqlParameter[]
+                {
+                    new SqlParameter("@vacantJobId", entity.Id),
+                    new SqlParameter("@categoryId", entity.CategoryId),
+                    new SqlParameter("@specializationid", entity.SpecializationId),
+                    new SqlParameter("@jobAdvertTitle", entity.Title),
+                    new SqlParameter("@jobAdvertSummary", entity.Summary),
+                    new SqlParameter("@jobAdvertRegistrationDateTime", entity.RegistrationDateTime),
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spCreateJobAdvert]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+
+                    try
+                    {
+                        await conn.OpenAsync();
+
+                        entityId = (int)await cmd.ExecuteScalarAsync();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            if (entityId != 0)
+            {
+                return await GetByIdAsync(entityId);
+            }
+
+            return null;
+        }
+
+
+        public async Task<List<IJobAdvert>> GetAllAsync()
+        {
+            List<IJobAdvert> jobAdverts = new();
+
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Basic))
+            {
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spGetJobAdverts]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+
+                    try
+                    {
+                        using (var reader = await cmd.ExecuteReaderAsync())
+                        {
+                            if (!reader.HasRows) return null;
+
+                            while (await reader.ReadAsync())
+                            {
+                                var jobAdvert = new JobAdvert
+                                {
+                                    Id = reader.GetInt32(0),
+                                    VacantJobId = reader.GetInt32(0),
+                                    CategoryId = reader.GetInt32(6),
+                                    SpecializationId = reader.GetInt32(8),
+                                    Title = reader.GetString(11),
+                                    Summary = reader.GetString(12),
+                                    RegistrationDateTime = reader.GetDateTime(13)
+
+                                };
+
+                                jobAdverts.Add(jobAdvert);
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return jobAdverts;
         }
 
         public Task<List<JobAdvert>> GetAllUncategorized()
@@ -22,34 +111,230 @@ namespace JobAgentClassLibrary.Common.JobAdverts.Repositories
             throw new NotImplementedException();
         }
 
-        public Task<IJobAdvert> GetByIdAsync(int id)
+
+        public async Task<IJobAdvert> GetByIdAsync(int id)
         {
-            throw new NotImplementedException();
+            IJobAdvert jobAdvert = null;
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Basic))
+            {
+                var values = new SqlParameter[]
+                {
+                        new SqlParameter("@id", id)
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spGetJobAdvertById]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+
+                    try
+                    {
+                        using (var reader = await cmd.ExecuteReaderAsync())
+                        {
+                            if (!reader.HasRows) return null;
+
+                            while (await reader.ReadAsync())
+                            {
+                                jobAdvert = new JobAdvert
+                                {
+                                    Id = reader.GetInt32(0),
+                                    VacantJobId = reader.GetInt32(0),
+                                    CategoryId = reader.GetInt32(6),
+                                    SpecializationId = reader.GetInt32(8),
+                                    Title = reader.GetString(11),
+                                    Summary = reader.GetString(12),
+                                    RegistrationDateTime = reader.GetDateTime(13)
+                                };
+
+                            }
+                        }
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+            return jobAdvert;
         }
 
-        public Task<int> GetJobAdvertCountByCategoryId(int id)
+
+        public async Task<int> GetJobAdvertCountByCategoryId(int id)
         {
-            throw new NotImplementedException();
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Basic))
+            {
+
+                SqlParameter outputParameter = new() { Direction = ParameterDirection.Output, ParameterName = "@countByCategory", SqlDbType = SqlDbType.Int };
+
+                SqlParameter[] values = new[]
+                {
+                    new SqlParameter("@categoryId", id),
+                    outputParameter
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spGetTotalJobAdvertCountByCategoryId]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+                    await cmd.ExecuteNonQueryAsync();
+                }
+
+                bool parsedOutput = int.TryParse(values[1].Value.ToString(), out int count);
+                if (parsedOutput)
+                {
+                    return count;
+                }
+
+                return 0;
+            }
         }
 
-        public Task<int> GetJobAdvertCountBySpecializationId(int id)
+
+        public async Task<int> GetJobAdvertCountBySpecializationId(int id)
         {
-            throw new NotImplementedException();
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Basic))
+            {
+
+                SqlParameter outputParameter = new() { Direction = ParameterDirection.Output, ParameterName = "@countByCategory", SqlDbType = SqlDbType.Int };
+
+                SqlParameter[] values = new[]
+                {
+                    new SqlParameter("@specializationId", id),
+                    outputParameter
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spGetTotalJobAdvertCountBySpecializationId]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+                    await cmd.ExecuteNonQueryAsync();
+                }
+
+                bool parsedOutput = int.TryParse(values[1].Value.ToString(), out int count);
+                if (parsedOutput)
+                {
+                    return count;
+                }
+
+                return 0;
+            }
         }
 
-        public Task<int> GetJobAdvertCountByUncategorized()
+
+        public async Task<int> GetJobAdvertCountByUncategorized()
         {
-            throw new NotImplementedException();
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Basic))
+            {
+
+                SqlParameter outputParameter = new() { Direction = ParameterDirection.Output, ParameterName = "@countByCategory", SqlDbType = SqlDbType.Int };
+
+                SqlParameter[] values = new[]
+                {
+                    outputParameter
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spGetTotalJobAdvertCountByNonCategorized]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+                    await cmd.ExecuteNonQueryAsync();
+                }
+
+                bool parsedOutput = int.TryParse(values[1].Value.ToString(), out int count);
+                if (parsedOutput)
+                {
+                    return count;
+                }
+
+                return 0;
+            }
         }
 
-        public Task<bool> RemoveAsync(IJobAdvert entity)
+
+        public async Task<bool> RemoveAsync(IJobAdvert entity)
         {
-            throw new NotImplementedException();
+            int entityId = 0;
+
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Delete))
+            {
+                var values = new SqlParameter[]
+                {
+                        new SqlParameter("@id", entity.Id)
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spRemoveJobAdvert]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+
+                    try
+                    {
+                        await conn.OpenAsync();
+
+                        entityId = (int)await cmd.ExecuteScalarAsync();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            if (entityId != 0)
+            {
+                return true;
+            }
+
+            return false;
         }
 
-        public Task<IJobAdvert> UpdateAsync(IJobAdvert entity)
+
+        public async Task<IJobAdvert> UpdateAsync(IJobAdvert entity)
         {
-            throw new NotImplementedException();
+            int entityId = 0;
+
+            using (var conn = _sqlDbManager.GetSqlConnection(DbConnectionType.Update))
+            {
+                var values = new SqlParameter[]
+                {
+                    new SqlParameter("@vacantJobId", entity.Id),
+                    new SqlParameter("@categoryId", entity.CategoryId),
+                    new SqlParameter("@specializationid", entity.SpecializationId),
+                    new SqlParameter("@jobAdvertTitle", entity.Title),
+                    new SqlParameter("@jobAdvertSummary", entity.Summary),
+                    new SqlParameter("@jobAdvertRegistrationDateTime", entity.RegistrationDateTime)
+                };
+
+                using (var cmd = conn.CreateCommand())
+                {
+                    cmd.CommandText = "[JA.spUpdateJobAdvert]";
+                    cmd.CommandType = CommandType.StoredProcedure;
+                    cmd.Parameters.AddRange(values);
+
+                    try
+                    {
+                        await conn.OpenAsync();
+
+                        entityId = (int)await cmd.ExecuteScalarAsync();
+                    }
+                    catch (Exception)
+                    {
+                        throw;
+                    }
+                }
+            }
+
+            if (entityId != 0)
+            {
+                return await GetByIdAsync(entityId);
+            }
+
+            return null;
         }
     }
 }
