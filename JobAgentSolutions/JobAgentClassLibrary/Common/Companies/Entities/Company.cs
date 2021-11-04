1683383655d1ff07a85230afe5c09bc39eca6fc5
Tobias Rosenvinge <TobiasRosenvinge@hotmail.com>
2021-11-04 15:57:58 +0100

    Removed Properties from Company.

--- JobAgentSolutions/JobAgentClassLibrary/Common/Companies/Entities/Company.cs
+++ JobAgentSolutions/JobAgentClassLibrary/Common/Companies/Entities/Company.cs
@@ -3,8 +3,6 @@
     public class Company : ICompany
     {
         public int Id { get; set; }
-        public int CVR { get; set; }
         public string Name { get; set; }
-        public string ContactPerson { get; set; }
     }
 }
--- JobAgentSolutions/JobAgentClassLibrary/Common/Companies/Entities/ICompany.cs
+++ JobAgentSolutions/JobAgentClassLibrary/Common/Companies/Entities/ICompany.cs
@@ -5,8 +5,6 @@ namespace JobAgentClassLibrary.Common.Companies.Entities
 {
     public interface ICompany : IAggregateRoot, IEntity<int>
     {
-        public int CVR { get; }
         public string Name { get; }
-        public string ContactPerson { get; }
     }
 }
