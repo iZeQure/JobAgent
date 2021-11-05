4f0d82c15947ec6852ea39ca5acee746944b1e79
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-05 09:58:21 +0100

    Implemented CompanyEntityFactory

---
+++ JobAgentSolutions/JobAgentClassLibrary/Common/Companies/Factory/CompanyEntityFactory.cs
@@ -0,0 +1,26 @@
+﻿using JobAgentClassLibrary.Common.Companies.Entities;
+using JobAgentClassLibrary.Common.Companies.Entities.EntityMaps;
+using JobAgentClassLibrary.Core.Factories;
+using JobAgentClassLibrary.Core.Repositories;
+using System;
+
+namespace JobAgentClassLibrary.Common.Companies.Factory
+{
+    public class CompanyEntityFactory : FactoryParser, IFactory
+    {
+        public IAggregateRoot CreateEntity(string paramName, params object[] entityValues) => paramName switch
+        {
+            nameof(Company) => new Company
+            {
+                Id = ParseEntityValueToInt(entityValues[0]),
+                Name = ParseEntityValueToString(entityValues[1])
+            },
+            nameof(CompanyInformation) => new CompanyInformation
+            {
+                CompanyId = ParseEntityValueToInt(entityValues[0]),
+                CompanyName = ParseEntityValueToString(entityValues[1]),
+            },
+            _ => throw new ArgumentOutOfRangeException(nameof(paramName), paramName, "Coudln't create type. Out of range.")
+        };
+    }
+}
