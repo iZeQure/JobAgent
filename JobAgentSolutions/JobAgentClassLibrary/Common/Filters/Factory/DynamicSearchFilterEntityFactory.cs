29256982fa4f81c1c0a1ac521fbd19e5f70533fd
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-05 10:12:56 +0100

    Implemented the 3 filter factories

---
+++ JobAgentSolutions/JobAgentClassLibrary/Common/Filters/Factory/DynamicSearchFilterEntityFactory.cs
@@ -0,0 +1,31 @@
+﻿using JobAgentClassLibrary.Common.Filters.Entities;
+using JobAgentClassLibrary.Common.Filters.Entities.EntityMaps;
+using JobAgentClassLibrary.Core.Factories;
+using JobAgentClassLibrary.Core.Repositories;
+using System;
+
+namespace JobAgentClassLibrary.Common.Filters.Factory
+{
+    public class DynamicSearchFilterEntityFactory : FactoryParser, IFactory
+    {
+        public IAggregateRoot CreateEntity(string paramName, params object[] entityValues) => paramName switch
+        {
+            nameof(DynamicSearchFilter) => new DynamicSearchFilter
+            {
+                Id = ParseEntityValueToInt(entityValues[0]),
+                SpecializationId = ParseEntityValueToInt(entityValues[1]),
+                CategoryId = ParseEntityValueToInt(entityValues[2]),
+                Key = ParseEntityValueToString(entityValues[3])
+            },
+            nameof(DynamicSearchFilterInformation) => new DynamicSearchFilterInformation
+            {
+                DynamicSearchFilterId = ParseEntityValueToInt(entityValues[0]),
+                SpecializationId = ParseEntityValueToInt(entityValues[1]),
+                CategoryId = ParseEntityValueToInt(entityValues[2]),
+                DynamicSearchFilterKey = ParseEntityValueToString(entityValues[3])
+            },
+            _ => throw new ArgumentOutOfRangeException(nameof(paramName), paramName, "Coudln't create type. Out of range.")
+
+        };
+    }
+}
---
+++ JobAgentSolutions/JobAgentClassLibrary/Common/Filters/Factory/StaticSearchFilterEntityFactory.cs
@@ -0,0 +1,29 @@
+﻿using JobAgentClassLibrary.Common.Filters.Entities;
+using JobAgentClassLibrary.Common.Filters.Entities.EntityMaps;
+using JobAgentClassLibrary.Core.Factories;
+using JobAgentClassLibrary.Core.Repositories;
+using System;
+
+namespace JobAgentClassLibrary.Common.Filters.Factory
+{
+    public class StaticSearchFilterEntityFactory : FactoryParser, IFactory
+    {
+        public IAggregateRoot CreateEntity(string paramName, params object[] entityValues) => paramName switch
+        {
+            nameof(StaticSearchFilter) => new StaticSearchFilter
+            {
+                Id = ParseEntityValueToInt(entityValues[0]),
+                FilterType = new() { Id = ParseEntityValueToInt(entityValues[1]) },
+                Key = ParseEntityValueToString(entityValues[2])
+            },
+            nameof(StaticSearchFilterInformation) => new StaticSearchFilterInformation
+            {
+                StaticSearchFilterId = ParseEntityValueToInt(entityValues[0]),
+                FilterTypeId = ParseEntityValueToInt(entityValues[1]),
+                StaticSearchFilterKey = ParseEntityValueToString(entityValues[2])
+            },
+            _ => throw new ArgumentOutOfRangeException(nameof(paramName), paramName, "Coudln't create type. Out of range.")
+        };
+
+    }
+}
