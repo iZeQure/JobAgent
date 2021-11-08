d1727e66c064deb1703a03f7a8a3bafd7293f527
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-08 12:15:57 +0100

    Update UserEntityFactory to include the creation of AuthUser

--- JobAgentSolutions/JobAgentClassLibrary/Common/Users/Factory/UserEntityFactory.cs
+++ JobAgentSolutions/JobAgentClassLibrary/Common/Users/Factory/UserEntityFactory.cs
@@ -2,6 +2,7 @@
 using JobAgentClassLibrary.Core.Factories;
 using JobAgentClassLibrary.Core.Repositories;
 using System;
+using System.Linq;
 
 namespace JobAgentClassLibrary.Common.Users.Factory
 {
@@ -12,11 +13,23 @@ namespace JobAgentClassLibrary.Common.Users.Factory
             nameof(User) => new User
             {
                 Id = ParseValue<int>(entityValues[0]),
-                RoleId = ParseValue<int>(entityValues[1]) ,
+                RoleId = ParseValue<int>(entityValues[1]),
+                LocationId = ParseValue<int>(entityValues[2]),
+                FirstName = ParseValue<string>(entityValues[3]),
+                LastName = ParseValue<string>(entityValues[4]),
+                Email = ParseValue<string>(entityValues[5])
+            },
+            nameof(AuthUser) => new AuthUser
+            {
+                Id = ParseValue<int>(entityValues[0]),
+                RoleId = ParseValue<int>(entityValues[1]),
                 LocationId = ParseValue<int>(entityValues[2]),
                 FirstName = ParseValue<string>(entityValues[3]),
                 LastName = ParseValue<string>(entityValues[4]),
                 Email = ParseValue<string>(entityValues[5]),
+                AccessToken = ParseValue<string>(6)
+                //Salt = ParseValue<string>(entityValues.ElementAtOrDefault(7) ?? string.Empty)
+                //Password = ParseValue<string>(entityValues.ElementAtOrDefault(8) ?? string.Empty)
             },
             _ => throw new ArgumentOutOfRangeException(nameof(paramName), paramName, "Coudln't create type. Out of range.")
         };
