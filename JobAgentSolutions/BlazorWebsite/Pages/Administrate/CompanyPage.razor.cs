316fcf01dfccb4faba4ddf2bf568e559bb4b6bbb
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-10 13:12:16 +0100

    14/PageUpdate
    Removed unused usings

--- JobAgentSolutions/BlazorWebsite/Pages/Administrate/CompanyPage.razor.cs
+++ JobAgentSolutions/BlazorWebsite/Pages/Administrate/CompanyPage.razor.cs
@@ -6,8 +6,6 @@ using Microsoft.JSInterop;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading;
 using System.Threading.Tasks;
 
 namespace BlazorWebsite.Pages.Administrate
