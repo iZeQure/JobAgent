8657f337c0214ad5d84e9a2e34055bedbb2ea0dd
Tobias Rosenvinge <37138833+iZeQure@users.noreply.github.com>
2021-11-11 12:58:43 +0100

    Revert "25/blazor website/form models"

--- JobAgentSolutions/BlazorWebsite/Data/FormModels/AccountProfileDiverseModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace BlazorWebsite.Data.FormModels
-{
-    public class AccountProfileDiverseModel
-    {
-        public int ConsultantAreaIdToBeAssigned { get; set; }
-        public int ConsultantAreaIdToBeRemoved { get; set; }
-    }
-}
