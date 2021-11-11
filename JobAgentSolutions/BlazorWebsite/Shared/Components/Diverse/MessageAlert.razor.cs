e5e4e0c84751434ed487ff8e1fa242dd0eed730a
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-11 14:25:53 +0100

    30/ImplementedComponents

---
+++ JobAgentSolutions/BlazorWebsite/Shared/Components/Diverse/MessageAlert.razor.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebsite.Shared.Components.Diverse
+{
+    public partial class MessageAlert : ComponentBase
+    {
+        [Parameter] public string Message { get; set; } = string.Empty;
+        [Parameter] public string MessageOptional { get; set; } = string.Empty;
+        [Parameter] public AlertType Alert { get; set; }
+        [Parameter] public bool IsLoading { get; set; } = false;
+        [Parameter] public bool FullWidth { get; set; } = true;
+
+        public string Container
+        {
+            get
+            {
+                if (FullWidth)
+                    return "container-fluid";
+                else
+                    return "container";
+            }
+        }
+
+        protected override void OnInitialized()
+        {
+            StateHasChanged();
+        }
+
+        public enum AlertType { Info, Warning, Error, Success }
+    }
+}
---
+++ JobAgentSolutions/BlazorWebsite/Shared/Components/Modals/CompanyModals/EditCompanyModal.razor.cs
@@ -0,0 +1,49 @@
+﻿using BlazorWebsite.Data.FormModels;
+using BlazorWebsite.Data.Providers;
+using JobAgentClassLibrary.Common.Companies;
+using JobAgentClassLibrary.Common.Companies.Entities;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+using System.Threading.Tasks;
+
+namespace BlazorWebsite.Shared.Components.Modals.CompanyModals
+{
+    public partial class EditCompanyModal : ComponentBase
+    {
+        [Parameter] public CompanyModel _companyModel { get; set; }
+        [Inject] protected ICompanyService CompanyService { get; set; }
+        [Inject] protected IRefreshProvider RefreshProvider { get; set; }
+        [Inject] protected IJSRuntime JSRuntime { get; set; }
+
+        private bool _isProcessing = false;
+        private bool _showError = false;
+        private string _errorMessage = string.Empty;
+
+        private async Task OnValidSubmit_UpdateCompany()
+        {
+            try
+            {
+                _isProcessing = true;
+
+                Company company = new()
+                {
+                    Id = _companyModel.CompanyId,
+                    Name = _companyModel.Name
+                };
+
+                await CompanyService.UpdateAsync(company);
+
+                RefreshProvider.CallRefreshRequest();
+
+                await JSRuntime.InvokeVoidAsync("toggleModalVisibility", "ModalEditCompany");
+                await JSRuntime.InvokeVoidAsync("onInformationChangeAnimateTableRow", $"{_companyModel.CompanyId}");
+
+            }
+            finally
+            {
+                _isProcessing = false;
+                StateHasChanged();
+            }
+        }
+    }
+}
