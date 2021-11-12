bc362ee608cb28b5c4bbb2199f065eece428698e
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-12 08:27:00 +0100

    30/ImplementRedirects
    Implemented AdminPageRedirect
    
    Implemented HomePageRedirect

---
+++ JobAgentSolutions/BlazorWebsite/Shared/Components/Redirects/AdminPageRedirect.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorWebsite.Shared.Components.Redirects
+{
+    /// <summary>
+    /// Handles the redirection to Admin Page.
+    /// </summary>
+    public class AdminPageRedirect : ComponentBase
+    {
+        [Inject]
+        protected NavigationManager NavigationManager { get; set; }
+
+        protected override void OnInitialized()
+        {
+            NavigationManager.NavigateTo("/admin", true);
+        }
+    }
+}
