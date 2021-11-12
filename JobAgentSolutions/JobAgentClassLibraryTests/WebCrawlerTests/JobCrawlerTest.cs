34dcd6e2e61be48fdbd450ef5d8fc0c0cae189bd
Kenneth Andersen (KENN229K) <kenn229k@zbc.dk>
2021-11-12 10:15:41 +0100

    Added test method
    made the first method to test still needs the logic and dependencies

--- JobAgentSolutions/JobAgentClassLibraryTests/WebCrawlerTests/JobCrawlerTest.cs
+++ JobAgentSolutions/JobAgentClassLibraryTests/WebCrawlerTests/JobCrawlerTest.cs
@@ -3,10 +3,15 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using WebCrawler.Crawler;
 
 namespace JobAgentClassLibraryTests.WebCrawlerTests
 {
-    class JobCrawlerTest
+    public class JobCrawlerTest
     {
+        public void GetDataVacantJobs_ShouldFillUpListWithHtmlDocuments()
+        {
+            LinkProvider
+        }
     }
 }
