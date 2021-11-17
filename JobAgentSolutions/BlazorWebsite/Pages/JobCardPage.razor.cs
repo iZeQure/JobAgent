17f0f1b817c676ea5d57f7032da314336166408f
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2021-11-17 13:42:58 +0100

    Master/JobCardUpdate
    Added references to CompanyService and VacantJobService to jobCardPage
    
    Hooked up the data needed for the jobcard so that it's displayed correctly

--- JobAgentSolutions/BlazorWebsite/Pages/JobCardPage.razor.cs
+++ JobAgentSolutions/BlazorWebsite/Pages/JobCardPage.razor.cs
@@ -1,8 +1,13 @@
-﻿using JobAgentClassLibrary.Common.JobAdverts;
+﻿using JobAgentClassLibrary.Common.Companies;
+using JobAgentClassLibrary.Common.Companies.Entities;
+using JobAgentClassLibrary.Common.JobAdverts;
 using JobAgentClassLibrary.Common.JobAdverts.Entities;
+using JobAgentClassLibrary.Common.VacantJobs;
+using JobAgentClassLibrary.Common.VacantJobs.Entities;
 using Microsoft.AspNetCore.Components;
 using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -12,10 +17,15 @@ namespace BlazorWebsite.Pages
     {
         [Parameter] public int CategoryId { get; set; }
         [Parameter] public int SpecializationId { get; set; }
+        [Inject] protected ICompanyService CompanyService { get; set; }
+        [Inject] protected IVacantJobService VacantJobService { get; set; }
         [Inject] protected IJobAdvertService JobAdvertService { get; set; }
 
         private IEnumerable<IJobAdvert> _jobAdverts = new List<JobAdvert>();
+        private IEnumerable<IVacantJob> _vacantJobs = new List<VacantJob>();
+        private IEnumerable<ICompany> _companies = new List<Company>();
         private IJobAdvert _jobAdvertDetails;
+        private CultureInfo cultureInfo = CultureInfo.CreateSpecificCulture("da-DK");
 
         private int _chosenCategoryId;
         private bool _isLoadingData = false;
@@ -46,6 +56,9 @@ namespace BlazorWebsite.Pages
             try
             {
                 _loadFailed = false;
+
+                _vacantJobs = await VacantJobService.GetAllAsync();
+                _companies = await CompanyService.GetAllAsync();
                 var jobAdverts = await JobAdvertService.GetJobAdvertsAsync();
 
                 if (CategoryId == 0 && SpecializationId == 0)
