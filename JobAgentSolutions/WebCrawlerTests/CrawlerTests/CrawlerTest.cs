4dd191708af450c41a76d018a9a22c001a5e8047
Kenneth Andersen (KENN229K) <kenn229k@zbc.dk>
2021-11-21 16:53:45 +0100

    Added Tests project for crawler
    Added a forlder and a test class

---
+++ JobAgentSolutions/WebCrawlerTests/CrawlerTests/CrawlerTest.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using WebCrawler.DataScrappers;
+using WebCrawler.Factories;
+using WebCrawler.Managers;
+using Xunit;
+using static WebCrawler.DataScrappers.CrawlerSettings;
+
+namespace WebCrawlerTests.CrawlerTests
+{
+    public class CrawlerTest
+    {
+
+        [Theory]
+        [InlineData("", PageDefinitions.praktikpladsen)]
+        public void SetCrawlerUrl(string url, PageDefinitions pageDefinition)
+        {
+            CrawlerManager crawlerManager = CrawlerFactory.GetCrawlerFactory.GetCrawlerManager();
+            crawlerManager.SetUrl(url, pageDefinition);
+
+
+        }
+    }
+}
