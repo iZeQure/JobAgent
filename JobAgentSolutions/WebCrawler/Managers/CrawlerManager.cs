337c1a3cecd9948cdcbc2af612469fb7a3bf7356
Kenneth Andersen (KENN229K) <kenn229k@zbc.dk>
2021-11-21 17:27:11 +0100

    Added some comments to crawlermanager

--- JobAgentSolutions/WebCrawler/Managers/CrawlerManager.cs
+++ JobAgentSolutions/WebCrawler/Managers/CrawlerManager.cs
@@ -9,7 +9,12 @@ namespace WebCrawler.Managers
     {
         private IHtmlSorter _sorter;
         private ICrawler _crawler;
-        
+
+        private List<string> _urlsToCrawl;
+
+        /// <summary>
+        /// Gets links to crawl from db
+        /// </summary>
         public void GetStartLink()
         {
             // Get links from db
@@ -19,23 +24,42 @@ namespace WebCrawler.Managers
         {
             _crawler = crawler;
             _sorter = sorter;
+            _urlsToCrawl = new List<string>();
         }
 
+        /// <summary>
+        /// Used to set the settings on the crawler
+        /// </summary>
+        /// <param name="settings"></param>
         public void SetCrawlerSettings(CrawlerSettings settings)
         {
             _crawler.SetCrawlerSettings(settings);
         }
 
+        /// <summary>
+        /// Sets the url to crawl on 
+        /// needs page definition to narrow the search on the page
+        /// </summary>
+        /// <param name="url"></param>
+        /// <param name="pageDefinition"></param>
         public void SetUrl(string url, CrawlerSettings.PageDefinitions pageDefinition)
         {
             _crawler.SetCrawlerUrl(url, pageDefinition);
         }
-         
+
+        /// <summary>
+        /// Used to get the links from the sorter
+        /// Returns empty list if HtmlArraySplitOn has not been run 
+        /// </summary>
+        /// <returns></returns>
         public List<string> GetLinksFound()
         {
             return _sorter.LinksFromSite;
         }
-
+        
+        /// <summary>
+        /// Starts the crawler loops through all links provided then stops
+        /// </summary>
         public void StarCrawler()
         {
             var hmltdocument = _crawler.Crawl();
