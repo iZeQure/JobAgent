a0e19d077439da9c3acb3a5b7ca146c593b0cc91
Jesper Klejs Deleurand <Jesperd55@gmail.com>
2022-01-14 13:10:10 +0100

    Master/ServiceExtension
    Implemented ServiceCollectionExtension to cleanup startup.cs

--- JobAgentSolutions/BlazorWebsite/Startup.cs
+++ JobAgentSolutions/BlazorWebsite/Startup.cs
@@ -1,42 +1,9 @@
 using Blazored.LocalStorage;
 using BlazorWebsite.Data.Providers;
 using BlazorWebsite.Data.Services;
-using JobAgentClassLibrary.Common.Areas;
-using JobAgentClassLibrary.Common.Areas.Factory;
-using JobAgentClassLibrary.Common.Areas.Repositories;
-using JobAgentClassLibrary.Common.Categories;
-using JobAgentClassLibrary.Common.Categories.Factory;
-using JobAgentClassLibrary.Common.Categories.Repositories;
-using JobAgentClassLibrary.Common.Companies;
-using JobAgentClassLibrary.Common.Companies.Factory;
-using JobAgentClassLibrary.Common.Companies.Repositories;
-using JobAgentClassLibrary.Common.Filters;
-using JobAgentClassLibrary.Common.Filters.Factory;
-using JobAgentClassLibrary.Common.Filters.Repositories;
-using JobAgentClassLibrary.Common.JobAdverts;
-using JobAgentClassLibrary.Common.JobAdverts.Factory;
-using JobAgentClassLibrary.Common.JobAdverts.Repositories;
-using JobAgentClassLibrary.Common.JobPages;
-using JobAgentClassLibrary.Common.JobPages.Factory;
-using JobAgentClassLibrary.Common.JobPages.Repositories;
-using JobAgentClassLibrary.Common.Locations;
-using JobAgentClassLibrary.Common.Locations.Factory;
-using JobAgentClassLibrary.Common.Locations.Repositories;
-using JobAgentClassLibrary.Common.Roles;
-using JobAgentClassLibrary.Common.Roles.Factory;
-using JobAgentClassLibrary.Common.Roles.Repositories;
-using JobAgentClassLibrary.Common.Users;
-using JobAgentClassLibrary.Common.Users.Factory;
-using JobAgentClassLibrary.Common.Users.Repositories;
-using JobAgentClassLibrary.Common.VacantJobs;
-using JobAgentClassLibrary.Common.VacantJobs.Factory;
-using JobAgentClassLibrary.Common.VacantJobs.Repositories;
-using JobAgentClassLibrary.Core.Database.Factories;
 using JobAgentClassLibrary.Core.Database.Managers;
 using JobAgentClassLibrary.Core.Settings;
-using JobAgentClassLibrary.Loggings;
-using JobAgentClassLibrary.Loggings.Factory;
-using JobAgentClassLibrary.Loggings.Repositories;
+using JobAgentClassLibrary.Extensions.ServiceCollection;
 using JobAgentClassLibrary.Security.Access;
 using JobAgentClassLibrary.Security.Cryptography;
 using JobAgentClassLibrary.Security.Cryptography.Hashing;
@@ -78,20 +45,7 @@ namespace BlazorWebsite
 
 
             /* Factory Injections */
-            services.AddSingleton<ISqlDbFactory, SqlDbFactory>();
-            services.AddSingleton<AreaEntityFactory>();
-            services.AddSingleton<CategoryEntityFactory>();
-            services.AddSingleton<CompanyEntityFactory>();
-            services.AddSingleton<DynamicSearchFilterEntityFactory>();
-            services.AddSingleton<FilterTypeEntityFactory>();
-            services.AddSingleton<StaticSearchFilterEntityFactory>();
-            services.AddSingleton<JobAdvertEntityFactory>();
-            services.AddSingleton<JobPageEntityFactory>();
-            services.AddSingleton<LocationEntityFactory>();
-            services.AddSingleton<RoleEntityFactory>();
-            services.AddSingleton<UserEntityFactory>();
-            services.AddSingleton<VacantJobEntityFactory>();
-            services.AddSingleton<LogEntityFactory>();
+            services.AddFactories();
 
 
             /* Manager Injections */
@@ -100,36 +54,11 @@ namespace BlazorWebsite
 
 
             /* Repository Injections */
-            services.AddScoped<IUserRepository, UserRepository>();
-            services.AddScoped<ICompanyRepository, CompanyRepository>();
-            services.AddScoped<ILocationRepository, LocationRepository>();
-            services.AddScoped<IAreaRepository, AreaRepository>();
-            services.AddScoped<IRoleRepository, RoleRepository>();
-            services.AddScoped<IJobAdvertRepository, JobAdvertRepository>();
-            services.AddScoped<IJobPageRepository, JobPageRepository>();
-            services.AddScoped<IVacantJobRepository, VacantJobRepository>();
-            services.AddScoped<ICategoryRepository, CategoryRepository>();
-            services.AddScoped<IDynamicSearchFilterRepository, DynamicSearchFilterRepository>();
-            services.AddScoped<IStaticSearchFilterRepository, StaticSearchFilterRepository>();
-            services.AddScoped<IFilterTypeRepository, FilterTypeRepository>();
-            services.AddScoped<ISpecializationRepository, SpecializationRepository>();
-            services.AddScoped<ILoggingRepository, DbLogRepository>();
+            services.AddRepositories();
 
 
             /* Service Injections */
-            services.AddScoped<IAreaService, AreaService>();
-            services.AddScoped<IRoleService, RoleService>();
-            services.AddScoped<ILocationService, LocationService>();
-            services.AddScoped<ICompanyService, CompanyService>();
-            services.AddScoped<ICategoryService, CategoryService>();
-            services.AddScoped<IDynamicSearchFilterService, DynamicSearchFilterService>();
-            services.AddScoped<IStaticSearchFilterService, StaticSearchFilterService>();
-            services.AddScoped<IFilterTypeService, FilterTypeService>();
-            services.AddScoped<IUserService, UserService>();
-            services.AddScoped<IJobAdvertService, JobAdvertService>();
-            services.AddScoped<IVacantJobService, VacantJobService>();
-            services.AddScoped<IJobPageService, JobPageService>();
-            services.AddScoped<ILogService, DbLogService>();
+            services.AddEntityServices();
             services.AddTransient<PaginationService>();
 
 
@@ -138,7 +67,7 @@ namespace BlazorWebsite
 
 
             /* Provider Injections */
-            services.AddScoped<IMessageClearProvider, MessageClearProvider>();
+            services.AddTransient<IMessageClearProvider, MessageClearProvider>();
             services.AddScoped<IRefreshProvider, RefreshProvider>();
             services.AddScoped<MyAuthStateProvider>();
             services.AddScoped<AuthenticationStateProvider>(provider =>
---
+++ JobAgentSolutions/JobAgentClassLibrary/Extensions/ServiceCollection/ServiceCollectionExtensions.cs
@@ -0,0 +1,92 @@
+﻿using JobAgentClassLibrary.Common.Areas;
+using JobAgentClassLibrary.Common.Areas.Factory;
+using JobAgentClassLibrary.Common.Areas.Repositories;
+using JobAgentClassLibrary.Common.Categories;
+using JobAgentClassLibrary.Common.Categories.Factory;
+using JobAgentClassLibrary.Common.Categories.Repositories;
+using JobAgentClassLibrary.Common.Companies;
+using JobAgentClassLibrary.Common.Companies.Factory;
+using JobAgentClassLibrary.Common.Companies.Repositories;
+using JobAgentClassLibrary.Common.Filters;
+using JobAgentClassLibrary.Common.Filters.Factory;
+using JobAgentClassLibrary.Common.Filters.Repositories;
+using JobAgentClassLibrary.Common.JobAdverts;
+using JobAgentClassLibrary.Common.JobAdverts.Factory;
+using JobAgentClassLibrary.Common.JobAdverts.Repositories;
+using JobAgentClassLibrary.Common.JobPages;
+using JobAgentClassLibrary.Common.JobPages.Factory;
+using JobAgentClassLibrary.Common.JobPages.Repositories;
+using JobAgentClassLibrary.Common.Locations;
+using JobAgentClassLibrary.Common.Locations.Factory;
+using JobAgentClassLibrary.Common.Locations.Repositories;
+using JobAgentClassLibrary.Common.Roles;
+using JobAgentClassLibrary.Common.Roles.Factory;
+using JobAgentClassLibrary.Common.Roles.Repositories;
+using JobAgentClassLibrary.Common.Users;
+using JobAgentClassLibrary.Common.Users.Factory;
+using JobAgentClassLibrary.Common.Users.Repositories;
+using JobAgentClassLibrary.Common.VacantJobs;
+using JobAgentClassLibrary.Common.VacantJobs.Factory;
+using JobAgentClassLibrary.Common.VacantJobs.Repositories;
+using JobAgentClassLibrary.Core.Database.Factories;
+using JobAgentClassLibrary.Loggings;
+using JobAgentClassLibrary.Loggings.Factory;
+using JobAgentClassLibrary.Loggings.Repositories;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace JobAgentClassLibrary.Extensions.ServiceCollection
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddEntityServices(this IServiceCollection services) 
+            => services
+            .AddScoped<IAreaService, AreaService>()
+            .AddScoped<IRoleService, RoleService>()
+            .AddScoped<ILocationService, LocationService>()
+            .AddScoped<ICompanyService, CompanyService>()
+            .AddScoped<ICategoryService, CategoryService>()
+            .AddScoped<IDynamicSearchFilterService, DynamicSearchFilterService>()
+            .AddScoped<IStaticSearchFilterService, StaticSearchFilterService>()
+            .AddScoped<IFilterTypeService, FilterTypeService>()
+            .AddScoped<IUserService, UserService>()
+            .AddScoped<IJobAdvertService, JobAdvertService>()
+            .AddScoped<IVacantJobService, VacantJobService>()
+            .AddScoped<IJobPageService, JobPageService>()
+            .AddScoped<ILogService, DbLogService>();
+
+        public static IServiceCollection AddRepositories(this IServiceCollection services) 
+            => services
+            .AddScoped<IUserRepository, UserRepository>()
+            .AddScoped<ICompanyRepository, CompanyRepository>()
+            .AddScoped<ILocationRepository, LocationRepository>()
+            .AddScoped<IAreaRepository, AreaRepository>()
+            .AddScoped<IRoleRepository, RoleRepository>()
+            .AddScoped<IJobAdvertRepository, JobAdvertRepository>()
+            .AddScoped<IJobPageRepository, JobPageRepository>()
+            .AddScoped<IVacantJobRepository, VacantJobRepository>()
+            .AddScoped<ICategoryRepository, CategoryRepository>()
+            .AddScoped<IDynamicSearchFilterRepository, DynamicSearchFilterRepository>()
+            .AddScoped<IStaticSearchFilterRepository, StaticSearchFilterRepository>()
+            .AddScoped<IFilterTypeRepository, FilterTypeRepository>()
+            .AddScoped<ISpecializationRepository, SpecializationRepository>()
+            .AddScoped<ILoggingRepository, DbLogRepository>();
+
+        public static IServiceCollection AddFactories(this IServiceCollection services)
+            => services
+                .AddSingleton<ISqlDbFactory, SqlDbFactory>()
+                .AddSingleton<AreaEntityFactory>()
+                .AddSingleton<CategoryEntityFactory>()
+                .AddSingleton<CompanyEntityFactory>()
+                .AddSingleton<DynamicSearchFilterEntityFactory>()
+                .AddSingleton<FilterTypeEntityFactory>()
+                .AddSingleton<StaticSearchFilterEntityFactory>()
+                .AddSingleton<JobAdvertEntityFactory>()
+                .AddSingleton<JobPageEntityFactory>()
+                .AddSingleton<LocationEntityFactory>()
+                .AddSingleton<RoleEntityFactory>()
+                .AddSingleton<UserEntityFactory>()
+                .AddSingleton<VacantJobEntityFactory>()
+                .AddSingleton<LogEntityFactory>();
+
+    }
+}
